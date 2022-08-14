6f5e0dcfdd364612ee3dd5fc31b949963198d677
toughC <76736351+toughC@users.noreply.github.com>
2022-08-14 23:37:25 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void){
+   
+   int n1=0,n2=0;
+   cin >> n1 >> n2;
+   cout << n1 + n2 << endl;
+   cout << n1 - n2 << endl;
+   cout << n1 * n2 << endl;
+   cout << n1 / n2 << endl;
+   cout << n1 % n2 << endl;
+
+return 0;
+}
